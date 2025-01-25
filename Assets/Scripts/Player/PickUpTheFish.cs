f5d5e1d7e4eb7346752b9fdb2c081c9602380a50
liberte <ym265022@dal.ca>
2025-01-25 14:32:37 -0400

    added a primary panel for esc key.

--- Assets/Scripts/Player/PickUpTheFish.cs
+++ Assets/Scripts/Player/PickUpTheFish.cs
@@ -10,30 +10,28 @@ public class PickUpTheFish : MonoBehaviour
     
     // Start is called before the first frame update
     
-    public void Start(){
+    void Start(){
         GameObject FishTriggerObject = GameObject.Find("FishTrigger");
         fishDetector = FishTriggerObject.GetComponent<FishDetector>();
     }
-    public void Update(){
+    void Update(){
         if(fishDetector.canPickUpFish){
             if(Input.GetKeyDown("e")){
                 pickUp();
             }
         }
     }
-    public void onPickUpPause(){
+    void onPickUpPause(){
         Time.timeScale = 0;
         isPaused = true;
-        Debug.Log("paused");
         youFoundAFish.SetActive(true);
     }
-    public void onPickUpResume(){
+    void onPickUpResume(){
         Time.timeScale = 1;
         isPaused = false;
-        Debug.Log("resumed");
         youFoundAFish.SetActive(false);
     }
-    public void pickUp(){
+    void pickUp(){
         if(isPaused){
             onPickUpResume();
         }
