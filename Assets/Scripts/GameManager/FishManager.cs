90ebdc804b8617928ca96232cdbb986fc3e52401
LiamOakey <liamoakey008@gmail.com>
2025-01-26 14:27:48 -0400

    updated fishManager
    Updated fishManager

--- Assets/Scripts/GameManager/FishManager.cs
+++ Assets/Scripts/GameManager/FishManager.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 namespace GameManager
 {
@@ -8,26 +10,36 @@ namespace GameManager
 
         public const string BLOBFISH = "Blobfish";
         public const string CRAB = "Crab";
-        public const string FALSE_BOAT_FISH = "False Boat Fish";
+        public const string FALSE_BOAR_FISH = "FalseBoarFish";
         public const string OARFISH = "Oarfish";
         public const string SALMON = "Salmon";
         public const string SNAIL = "Snail";
         public const string SQUID = "Squid";
         public const string URCHIN = "Urchin";
         public const string ANGLER = "Angler";
-        public const string 
+        public const string DOLPHIN = "Dolphin";
+        public const string PUFFER_FISH = "Pufferfish";
+        public const string SWORD_FISH = "Swordfish";
+
+
+
+        public const string ROCK = "Rock";
+
         
         private static Dictionary<string, bool> FishFound = new Dictionary<string, bool>()
         {
             {BLOBFISH, false},
             {CRAB, false},
-            {FALSE_BOAT_FISH, false},
+            {FALSE_BOAR_FISH, false},
             {OARFISH, false},
             {SALMON, false},
             {SNAIL, false},
             {SQUID, false},
             {URCHIN, false},
-            {ANGLER, false}
+            {ANGLER, false},
+            {DOLPHIN, false},
+            {PUFFER_FISH, false},
+            {SWORD_FISH, false},
         };
 
         public static void CollectFish(string fishName)
@@ -45,6 +57,10 @@ namespace GameManager
             if (FishFound[BLOBFISH])
             {
                 Victory();
+            }
+
+            if(FishFound[ROCK]){
+                SceneManager.LoadScene("MainMenu");
             }
             
         }
